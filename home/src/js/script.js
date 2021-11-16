11e728bf41cb71fdc2acb016cde6a3dc792c317b
AdiSan103 <69831671+AdiSan103@users.noreply.github.com>
2021-11-16 17:53:53 +0800

    upload 1

---
+++ home/src/js/script.js
@@ -0,0 +1,52 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+        type   : 'loop',
+        perPage: 1,
+        focus  : 'center',
+        autoplay: true,
+      } );
+    splide.mount();
+  } );
+
+// hamburger menu
+
+function queryClass(theSelector) {
+  return document.querySelector(theSelector);
+}
+function addClass(theSelector, theClass) {
+  return queryClass(theSelector).classList.add(theClass);
+}
+
+function removeClass(theSelector, theClass) {
+  return queryClass(theSelector).classList.remove(theClass);
+}
+
+
+queryClass('.hamburger-menu').addEventListener('click', function() {
+  if(window.innerWidth < 992) {
+    //cek class d-none
+    if( queryClass('.navbar1-link').classList.contains('d-none') == true ) {
+      // navbar-link
+      removeClass('.navbar1-link','d-none');
+      addClass('.navbar1-link','d-flex');
+      setTimeout(() => {
+        addClass('.navbar1-link ul','true');
+      }, 100)
+      // hamburger menu
+      addClass('.hamburger-1', 'true');
+      addClass('.hamburger-2', 'true');
+      addClass('.hamburger-3', 'true');
+    }  else {
+      // navbar-link
+      removeClass('.navbar1-link','d-flex');
+      removeClass('.navbar1-link ul','true');
+      setTimeout(() => {
+        addClass('.navbar1-link','d-none');
+      }, 750)
+      // hamburger menu
+      removeClass('.hamburger-1', 'true');
+      removeClass('.hamburger-2', 'true');
+      removeClass('.hamburger-3', 'true');
+    }
+  }
+})
